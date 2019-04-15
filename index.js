4452d8fe6d6266caab778c38aa7eef5cc9bbe700
Benjamin <hoppeconnect@gmail.com>
2019-04-15 11:57:07 -0400

    probally broke something

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('roadster');
+
+let x = document.getElementsByClassName('landingToggle');
+let y = document.getElementsByClassName('baseSpecsToggle');
+
+function myFunction() {
+    x.style.display = "none";
+    y.style.display = "block";
+}
